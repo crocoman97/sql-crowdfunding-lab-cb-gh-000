364abb27c40915f6a085dbad2535f59b9440fda5
Rauof M <abdelrauof197@gmail.com>
2018-02-09 01:05:32 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category 
 TEXT, funding_goal TEXT, start_date DATE, end_date DATE)
-CREATE TABLE users (name TEXT, )
+CREATE TABLE users (name TEXT, age INTEGER)
 CREATE TABLE pledges ()
